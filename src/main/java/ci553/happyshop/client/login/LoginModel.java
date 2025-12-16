930c4ab94f9ca143537dd21e5a702c630118a6ff
zainab Ismail <Z.MohamedIsmail1@uni.brighton.ac.uk>
2025-12-16 16:44:59 +0000

    project set-up and initial login foundation error fix [initially changed original codebase to include screen for single user but then decided 2 screens was a better option]

--- src/main/java/ci553/happyshop/client/login/LoginModel.java
+++
@@ -1,8 +0,0 @@
-package ci553.happyshop.client.login;
-
-public class LoginModel {
-    // Temporary hardcoded login
-    public boolean authenticate(String username, String password) {
-        return "admin".equals(username) && "password123".equals(password);
-    }
-}
